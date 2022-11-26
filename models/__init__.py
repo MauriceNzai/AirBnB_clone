421b26ac62fb5800c1ba46b03dbac4b9f500d002
Maurice <maurice.nzai@gmail.com>
2022-11-26 07:34:14 +0000

    Update file

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Linking information between files
+
+Create on Tue Nov 22, 2022
+"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
